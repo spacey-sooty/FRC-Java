3e5b1e446ca2d88c8db9edaa82ad0f3eae85ca74
Isaac Turner <spacey_sooty@outlook.com>
2023-08-20 17:07:01 +0800

    declared motors and driver

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,29 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+  private static WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(4);
+  private static WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(12);
+
+  private static WPI_VictorSPX intakeMotor = new WPI_VictorSPX(11);
+
+  private static WPI_TalonSRX DrivebaseLeft1 = new WPI_TalonSRX(3);
+  private static WPI_TalonSRX DrivebaseLeft2 = new WPI_TalonSRX(7);
+  private static WPI_TalonSRX DrivebaseLeft3 = new WPI_TalonSRX(16);
+
+  private static WPI_TalonSRX DrivebaseRight1 = new WPI_TalonSRX(1);
+  private static WPI_TalonSRX DrivebaseRight2 = new WPI_TalonSRX(5);
+  private static WPI_TalonSRX DrivebaseRight3 = new WPI_TalonSRX(6);
+
+  private static XboxController driver = new XboxController(0);
+
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
