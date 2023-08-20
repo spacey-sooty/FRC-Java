a1c69e313aa5587ea07b22310e86685336ef6d6c
Isaac Turner <spacey_sooty@outlook.com>
2023-08-20 20:47:45 +0800

    declared variables

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,33 +4,37 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-  private static WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(4);
-  private static WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(12);
+private static WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(4);
+private static WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(12);
 
-  private static WPI_VictorSPX intakeMotor = new WPI_VictorSPX(11);
+private static WPI_VictorSPX intakeMotor = new WPI_VictorSPX(11);
 
-  private static WPI_TalonSRX DrivebaseLeft1 = new WPI_TalonSRX(3);
-  private static WPI_TalonSRX DrivebaseLeft2 = new WPI_TalonSRX(7);
-  private static WPI_TalonSRX DrivebaseLeft3 = new WPI_TalonSRX(16);
+private static WPI_TalonSRX DrivebaseLeft1 = new WPI_TalonSRX(3);
+private static WPI_TalonSRX DrivebaseLeft2 = new WPI_TalonSRX(7);
+private static WPI_TalonSRX DrivebaseLeft3 = new WPI_TalonSRX(16);
 
-  private static WPI_TalonSRX DrivebaseRight1 = new WPI_TalonSRX(1);
-  private static WPI_TalonSRX DrivebaseRight2 = new WPI_TalonSRX(5);
-  private static WPI_TalonSRX DrivebaseRight3 = new WPI_TalonSRX(6);
+private static WPI_TalonSRX DrivebaseRight1 = new WPI_TalonSRX(1);
+private static WPI_TalonSRX DrivebaseRight2 = new WPI_TalonSRX(5);
+private static WPI_TalonSRX DrivebaseRight3 = new WPI_TalonSRX(6);
 
-  private static XboxController driver = new XboxController(0);
+private static Compressor compressor = new Compressor(6, edu.wpi.first.wpilibj.PneumaticsModuleType.CTREPCM);
+private static DoubleSolenoid solenoid = new DoubleSolenoid(6, edu.wpi.first.wpilibj.PneumaticsModuleType.CTREPCM, 0, 1);
 
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+private static XboxController driver = new XboxController(0);
 
-  @Override
+private static final String kDefaultAuto = "Default";
+private static final String kCustomAuto = "My Auto";
+private String m_autoSelected;
+private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+@Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
